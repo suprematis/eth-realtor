eb2a152a27284cbf33347d97bdfbf84665151206
Andres Salgado <as+github@andresalgado.com>
2018-04-05 14:58:54 -0700

    Fixed property card title alignment and css for small screens.

--- src/components/Property.js
+++ src/components/Property.js
@@ -6,29 +6,32 @@ class Property extends React.Component {
   render() {
     const {HASH, LON, LAT, NUMBER, STREET, POSTCODE} = this.props.details;
     return (
-      // <div className="row">
-        <div className="col s6">
+      <div className="property-card">
+        <div className="col s12 m6 l6">
           <div className="card">
             <div className="card-image">
               <img className="activator" alt="" src="https://placeimg.com/320/240/arch" />
             </div>
             <div className="card-content">
-              <span className="card-title activator grey-text text-darken-4">{NUMBER} {STREET}</span>
+              <div className="row">
+                <div className="col s1"><i className="material-icons small">home</i></div>
+                <div className="col s11"><span className="card-title grey-text text-darken-4">{NUMBER} {STREET}</span></div>
+              </div>
               <li className="menu-property">
-              <p>
-                <span>Longitude: {LON}</span><br/>
-                <span>Latitude: {LAT}</span><br/>
-                <span>{NUMBER} {STREET}</span><br/>
-                <span>Zipcode: {POSTCODE}</span>
-              </p>
-              {/* <h3 className="property-name">
+                <p>
+                  {/* <span>Longitude: {LON}</span><br/>
+                  <span>Latitude: {LAT}</span><br/> */}
+                  <span>{NUMBER} {STREET}</span><br/>
+                  <span>{POSTCODE}</span>
+                </p>
+                {/* <h3 className="property-name">
                   <span className="price"></span>
-              </h3> */}
-            </li>
+                </h3> */}
+              </li>
+            </div>
           </div>
         </div>
-        </div>
-      // </div>
+      </div>
         )
   }
 }
